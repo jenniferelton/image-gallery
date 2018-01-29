2ef7024ebc9a030a1512c9357a1f4fda60173d05
Jennifer Elton <justjenny40@hotmail.com>
2018-01-28 17:14:58 -0800

    I hope this works

--- designers-spring-summer/src/services/cloudinary.js
+++
@@ -1,5 +0,0 @@
-const FETCH_URL = 'http://res.cloudinary.com/jennys-alchemy-code-lab/image/fetch';
-
-export const getUrl = (url, options = '') => {
-  return `${FETCH_URL}/${options}/${encodeURIComponent(url)}`;
-};
